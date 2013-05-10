f7e5a28a34004f9cfd40cb5920418de7c4fa7505
Cameron Burton <omegatwig@gmail.com>
2013-05-10 19:40:28 -0400

    Added execute functionality
    Execute works with pickled network and testing data

--- execute.py
+++ execute.py
@@ -5,18 +5,50 @@
 
 import cPickle
 import sys
+import csv
 
 from network import *
 from node import *
 
+def getData(dataFile):
+
+    trainingData = []
+
+    with open (dataFile, 'rb') as csvfile:
+        inReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in inReader:
+            tempRow = [float(i) for i in row]
+            trainingData.append(tempRow)
+
+    return trainingData
+
+
+# def main():
+	# testDataList = []
+	
+    # if len(sys.argv) != 3:
+        # print "Error, wrong number of arguments"
+        # return
+
+	# net = cPickle.load(file(sys.argv[2], 'r'))	
+	
+	# testDataList = self.getData(sys.argv[1])
+    
+    ##TODO: Run testing functions
+    # net.TestNetwork(testDataList, "")
+
+
 def main():
-    if len(sys.argv) != 2:
-        print "Error, wrong number of arguments"
-        return
-
-    net = cPickle.load(sys.argv[1])
-    #TODO: Run testing functions
-    pass
-
+	testDataList = []
+	
+	if len(sys.argv) != 3:
+		print "Error, wrong number of args"
+		return
+		
+	net = cPickle.load(file(sys.argv[2], 'r'))
+	
+	testDataList = getData(sys.argv[1])
+	
+	net.TestNetwork(testDataList, "")
 if __name__ == '__main__':
     main()
